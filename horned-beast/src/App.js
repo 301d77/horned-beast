b0f3769e61f584abf250acbb1ac23ba48307b0e5
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-08-16 18:11:41 -0700

    added hornedbeast things

--- horned-beast/src/App.js
+++ horned-beast/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="Horned Beasts" />
+      <Main />
+      <Footer author="Haustin Kimbrough"/>
+    </>
   );
 }
 
+function Header(props) {
+  return (
+    <h1>{props.title}</h1>
+  )
+}
+
+function Main() {
+  return (
+    <HornedBeast title="Horned Beasts" imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" description="beasts"/>
+  )
+}
+
+function HornedBeast(props) {
+  return (
+    <>
+    <h2>{props.title}</h2>
+    <img src={props.imageUrl} title={props.title} alt={props.description} />
+    <p>{props.description}</p>
+    </>
+  )
+}
+
+function Footer(props){
+  return(
+    <p>{props.author}</p>
+  )
+}
 export default App;
